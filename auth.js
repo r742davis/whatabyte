1664314006a6eb7999ceef07178ef14b1490e1b1
Richard Davis <richard.trow.davis@gmail.com>
2020-04-10 20:33:04 -0400

    Created the auth0 callback router

---
+++ auth.js
@@ -0,0 +1,51 @@
+// auth.js
+
+/**
+ * Required External Modules
+ */
+
+const express = require("express");
+const router = express.Router();
+const passport = require("passport");
+const util = require("util");
+const url = require("url");
+const querystring = require("querystring");
+
+require("dotenv").config();
+
+/**
+ * Routes Definitions
+ */
+
+router.get(
+  "/login",
+  passport.authenticate("auth0", {
+    scope: "openid email profile",
+  }),
+  (req, res) => {
+    res.redirect("/");
+  }
+);
+
+router.get("/callback", (req, res, next) => {
+  passport.authenticate("auth0", (err, user, info) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return res.redirect("/login");
+    }
+    req.logIn(user, (err) => {
+      if (err) {
+        return next(err);
+      }
+      const returnTo = req.session.returnTo;
+      delete req.session.returnTo;
+      res.redirect(returnTo || "/");
+    });
+  })(req, res, next);
+});
+
+/**
+ * Module Exports
+ */
