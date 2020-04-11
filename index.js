70e49e7f3ca5e6e339d3a95059ccbe5121696b6e
Richard Davis <richard.trow.davis@gmail.com>
2020-04-10 20:19:01 -0400

    Added Auth0 configuration to the index.js file, set up passport.js as well. Updated packages

--- index.js
+++ index.js
@@ -4,6 +4,12 @@
 const express = require("express");
 const path = require("path");
 
+const expressSession = require("express-session");
+const passport = require("passport");
+const Auth0Strategy = require("passport-auth0");
+
+require("dotenv").config();
+
 /**
  * App Variables
  */
@@ -11,11 +17,58 @@ const app = express();
 const port = process.env.PORT || "8000";
 
 /**
+ * Session Configuration
+ */
+
+const session = {
+  secret: "LoxodontaElephasMammuthusPalaeoloxodonPrimelephas",
+  cookie: {},
+  resave: false,
+  saveUninitialized: false,
+};
+
+if (app.get("env") === "production") {
+  // Serve secure cookirs, requires HTTPS
+  session.cookie.secure = true;
+}
+
+/**
+ * Passport Configuration
+ */
+
+const strategy = new Auth0Strategy(
+  {
+    domain: process.env.AUTH0_DOMAIN,
+    clientID: process.env.AUTH0_CLIENT_ID,
+    clientSecret: process.env.AUTH0_CLIENT_SECRET,
+    callbackURL:
+      process.env.AUTH0_CALLBACK_URL || "http://localhost:3000/callback",
+  },
+  function (accessToken, refreshToken, extraParams, profile, done) {
+    return done(null, profile);
+  }
+);
+
+/**
  *  App Configuration
  */
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use(expressSession(session));
+
+passport.use(strategy);
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
 
 /**
  * Routes Definitions
